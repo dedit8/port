b6740193ca28ab783d717f66ef85f8836e3b8ab4
dedit8 <guillermojesus15@gmail.com>
2022-05-10 21:11:43 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+
+require 'navar.php';
+
+require 'footer.php';
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="index.css">
+
+  </head>
+  <body>
+<div class="encavesado">
+  <h2>hola soy guillermo pegoraro. me dedico al desarrollo full stack aunque prefiero el back end</h2>
+</div>
+<h2>avilidades</h2>
+<div class="avil">
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img  src="https://alan-bersia.netlify.app/icons/html.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>html</h1>
+        <p>3 años</p>
+      </div>
+    </div>
+  </div>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img  src="https://alan-bersia.netlify.app/icons/css.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>css</h1>
+        <p>3 años</p>
+      </div>
+    </div>
+  </div>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img  src="https://alan-bersia.netlify.app/icons/git.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>git</h1>
+        <p>1 años</p>
+      </div>
+    </div>
+  </div>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img style="height: 60%;" src="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>php y sql</h1>
+        <p>3 años</p>
+      </div>
+    </div>
+  </div>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img  src="https://alan-bersia.netlify.app/icons/js.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>javascripy</h1>
+        <p>1 años</p>
+      </div>
+    </div>
+  </div>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img  src="https://alan-bersia.netlify.app/icons/jquery.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>jquery</h1>
+        <p>2 años</p>
+      </div>
+    </div>
+  </div>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img src="https://alan-bersia.netlify.app/icons/bootstrap.svg">
+      </div>
+      <div class="flip-card-back">
+        <h1>bootstrap</h1>
+        <p>3 años</p>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+</div>
+  </body>
+</html>
---
+++ navar.php
@@ -0,0 +1,29 @@
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.3/js/jquery.dataTables.js"></script>
+
+</head>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarCollapse">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="">inisio<span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">proyectos</a>
+    </li>
+
+  </ul>
+
+  </div>
+</nav>
